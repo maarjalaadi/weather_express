6d3770e740f2ee210cb20fc2d7c76f732e219921
maarjalaadi <145442157+maarjalaadi@users.noreply.github.com>
2023-12-05 18:09:43 +0200

    Add files via upload
    Added API data to project template file

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const app = express()
-
 const path = require('path')
+const fetch = require ('node-fetch')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+const key = 'fb3d255f31635c74fcb59fc27da14134';
+let city = '588335';
+
 app.get('/', function(req,res) {
-	res.render('index')
+	fetch(`https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${key}`)
+	.then((responce) => {
+		return responce.json()
+	})
+	.then((data) => {
+		let description = data.weather[0].description
+		let city = data.name
+		let temp = Math.round(parseFloat(data.main.temp)-273.15)
+	res.render('index', {
+		description: description,
+		city: city,
+		temp: temp
+		})  
+	}) 
+	 
 })
 
 app.listen(3000)
